28eea7703230aa1f5bdc0fe85b7d816eaf5574bb
hakonopheim <hakonopheim@asusphone.home>
2020-03-17 23:44:57 +0100

    Fikset ferdig endreProfil sidene på alle bruker roller

--- var/www/html/www/sda/reko/users/commerce/profile/profile.php
+++ var/www/html/www/sda/reko/users/commerce/profile/profile.php
@@ -9,7 +9,7 @@
     $userName=$del["userName"];
     $email=$del["email"];
     $image=$del["image"];
-
+    $phone=$del["phoneNumber"];
     $status=$_GET["status"];
 ?>
 
@@ -33,6 +33,8 @@
       <input type="text" id="lastName" name="lastName" value="<?php print($lastName); ?>"><br>
       <label for="lname">Email:</label><br>
       <input type="text" id="email" name="email" value="<?php print($email); ?>"><br>
+      <label for="lname">Telefon:</label><br>
+      <input type="text" id="phone" name="phone" value="<?php print($phone); ?>"><br>
       <img src="/www/sda/reko/img/users/<?php print($userID.'/'.$image);?>" height="100px"/>
       <input type="file" name="file"/><br>
       <input type="submit"  value="Endre" name="editProfile" id="editProfile">
@@ -41,10 +43,11 @@
     <?php
         if (isset($_POST ["editProfile"]))
         {
-          $userName=$_POST ["userName"];
-          $firstName=$_POST ["firstName"];
-          $lastName=$_POST ["lastName"];
-          $email=$_POST ["email"];
+          $userName1=$_POST ["userName"];
+          $firstName1=$_POST ["firstName"];
+          $lastName1=$_POST ["lastName"];
+          $email1=$_POST ["email"];
+          $phone1=$_POST ["phone"];
 
           $fileName=$_FILES ["file"]["name"];
 	        $fileType=$_FILES ["file"]["type"];
@@ -53,49 +56,54 @@
 
 
          $path = "/var/www/html/www/sda/reko/img/users/".$userID.'/';
-
+         
          if (!is_dir($path)) {
-          $oldmask = umask(0);
-          mkdir($path, 0777);   
-          umask($oldmask);
-              
-      }
+            $oldmask = umask(0);
+            mkdir($path, 0777);   
+            umask($oldmask);
+                
+        }
 
         if($fileName){
             
-        $newPath="/var/www/html/www/sda/reko/img/users/".$userID.'/'.$fileName;
+                $newPath="/var/www/html/www/sda/reko/img/users/".$userID.'/'.$fileName;
         
 
-            if($fileType != "image/gif" && $fileType != "image/jpeg" && $fileType != "image/jpg" && $fileType != "image/png" ){
-                print("<br><p>Filen må være et bilde.</p>");
-            }
-            if($profileIMG != $fileName){
+                if($fileType != "image/gif" && $fileType != "image/jpeg" && $fileType != "image/jpg" && $fileType != "image/png" )
+                {
+                    print("<br><p>Filen må være et bilde.</p>");
+                }
+                if($profileIMG != $fileName)
+                {
 
-                @unlink($path.$image);
-                move_uploaded_file($tmpName, $newPath) or die ("<br><p>Kunne ikke laste opp bilde til serveren!</p>"); 
+                    @unlink($path.$image);
+                    move_uploaded_file($tmpName, $newPath) or die ("<br><p>Kunne ikke laste opp bilde til serveren!</p>"); 
 
-                $sql= "UPDATE users SET firstName ='$firstName',lastName ='$lastName',email='$email',image='$fileName' WHERE userID='$userID';";
-                if(mysqli_query($db,$sql)){
-                  print("<meta http-equiv='refresh' content='0;URL=http://opheimpi.zapto.org/www/sda/reko/users/commerce/profile/profile.php?status=1'/>");
-				        }
-				        else{
-                  print("<meta http-equiv='refresh' content='0;URL=http://opheimpi.zapto.org/www/sda/reko/users/commerce/profile/profile.php?status=0'/>");
-					        unlink($newPath) or die ("<br><p>Ikke mulig å slette bilde på serveren igjen</p>");
-				        }
-            }
-            if($profileIMG == $fileName)
-            {
-                $sql= "UPDATE users SET firstName ='$firstName',lastName ='$lastName',email='$email' WHERE userID='$userID';";
-                mysqli_query($db,$sql) or die ("<br><p>Kunne ikke oppdatere databasen!</p>");
-                print("<meta http-equiv='refresh' content='0;URL=http://opheimpi.zapto.org/www/sda/reko/users/commerce/profile/profile.php?status=1'/>");
-            }
-          else
-            {
-              $sql= "UPDATE users SET firstName ='$firstName',lastName ='$lastName',email='$email' WHERE userID='$userID';";
-              mysqli_query($db,$sql) or die ("ikke mulig &aring; endre data i databasen");
-              print("<meta http-equiv='refresh' content='0;URL=http://opheimpi.zapto.org/www/sda/reko/users/commerce/profile/profile.php?status=1'/>");
-            }
+                    $sql= "UPDATE users SET firstName ='$firstName1',lastName ='$lastName',email='$email1',image='$fileName',phoneNumber='$phone1' WHERE userID='$userID';";
+                    if(mysqli_query($db,$sql))
+                        {
+                          print("<meta http-equiv='refresh' content='0;URL=http://opheimpi.zapto.org/www/sda/reko/users/moderator/profile.php?status=1'/>");
+                        }
+                    else
+                        {
+                          print("<meta http-equiv='refresh' content='0;URL=http://opheimpi.zapto.org/www/sda/reko/users/moderator/profile.php?status=0'/>");
+                          unlink($newPath) or die ("<br><p>Ikke mulig å slette bilde på serveren igjen</p>");
+                        }
+                }
+                if($profileIMG == $fileName)
+                  {
+                      $sql= "UPDATE users SET firstName ='$firstName1',lastName ='$lastName',email='$email1',phoneNumber='$phone1' WHERE userID='$userID';";
+                      mysqli_query($db,$sql) or die ("<br><p>Kunne ikke oppdatere databasen!</p>");
+                      print("<meta http-equiv='refresh' content='0;URL=http://opheimpi.zapto.org/www/sda/reko/users/moderator/profile.php?status=1'/>");
+                  }
+                
           }
+          else
+              {
+                $sql= "UPDATE users SET firstName ='$firstName1',lastName ='$lastName',email='$email1',phoneNumber='$phone1' WHERE userID='$userID';";
+                mysqli_query($db,$sql) or die ("ikke mulig &aring; endre data i databasen");
+                print("<meta http-equiv='refresh' content='0;URL=http://opheimpi.zapto.org/www/sda/reko/users/moderator/profile.php?status=1'/>");
+              }
         }
           ?>
   </div>
