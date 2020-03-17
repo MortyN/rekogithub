90e38220b797ade0b41d97238de23b55aa901384
hakonopheim <hakonopheim@asusphone.home>
2020-03-17 21:56:55 +0100

    bugfix

--- var/www/html/www/sda/reko/users/moderator/usersList.php
+++ var/www/html/www/sda/reko/users/moderator/usersList.php
@@ -3,21 +3,25 @@ include("control.php");
 ?>
 
 <div class="dashboard_content">
-
 <div class="innerContainerPrdOverview">
-    <script src ="ajax.js"></script>
-    <div class="form">
-        <form method="POST" action="">
-            <h3> Her kan du søke etter brukere</h3>
+    <div class="prdOverview_container">
+        <p>Under ser du en oversikt over alle brukere som er i denne reko ringen. Bruk søkefeltet for raskere navigering. </p>
+    </div>
+    <div class="prdOverview_container">
 
-            <p><a>Søkeord</a></p>
-            <input type="text" id="search" name="search" onKeyUp="show(this.value)" placeholder="Søkeord"/>
-            
+        <script src ="ajax.js"></script>
+        <div class="form">
+            <form method="POST" action="">
+                <h3> Her kan du søke etter brukere</h3>
+
+                <p><a>Søkeord</a></p>
+                <input type="text" id="search" name="search" onKeyUp="show(this.value)" placeholder="Søkeord"/>
+                
 
 
-            <div id="resultSearch">
-            
-            </div>
+                <div id="resultSearch">
+                
+                  </div>
 
         </form>
     </div>
