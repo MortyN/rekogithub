8dc79165e7d7dffdcb963f148488203e0c958871
hakonopheim <hakonopheim@asusphone.home>
2020-03-18 14:47:38 +0100

    test

--- var/www/html/www/sda/reko/users/moderator/editUser.php
+++ var/www/html/www/sda/reko/users/moderator/editUser.php
@@ -29,110 +29,110 @@ $selectedUserID = $_GET['userID'];
 <html>
 <div class="dashboard_content">
 
-<div class="messageBox">
-            <?php
-            if (isset($_GET['success'])) {
-                if ($_GET['success'] == "1") {
-                    echo "<p><strong style='color: red'> * When changing image information<br>all fields needs to be filled in!<br><br>Try again &#128540</strong></p>";
-                }
-                elseif ($_GET['success'] == "1") {
-                    echo "<p><strong style='color: mediumpurple'> * Image updated! &#128077</strong></p>";
-                }
-                elseif ($_GET['success'] == "1") {
-                    echo "<p><strong style='color: mediumpurple'> * Image updated! &#128077</strong></p>";
-                }
-            }
-            ?>
-        </div>
+  <div class="messageBox">
+              <?php
+              if (isset($_GET['success'])) {
+                  if ($_GET['success'] == "1") {
+                      echo "<p><strong style='color: red'> * When changing image information<br>all fields needs to be filled in!<br><br>Try again &#128540</strong></p>";
+                  }
+                  elseif ($_GET['success'] == "1") {
+                      echo "<p><strong style='color: mediumpurple'> * Image updated! &#128077</strong></p>";
+                  }
+                  elseif ($_GET['success'] == "1") {
+                      echo "<p><strong style='color: mediumpurple'> * Image updated! &#128077</strong></p>";
+                  }
+              }
+              ?>
+          </div>
 
 
 
-        </div>
+          
 
-<div class="profileeditor">
-    <form method="post" name="editProfile" action="">
+  <div class="profileeditor">
+      <form method="post" name="editProfile" action="">
 
-      <label for="fname">Brukernavn:</label><br>
-      <input type="text" id="userName" name="userName" value="<?php print($userName1); ?>" ><br>
-      <label for="fname">Fornavn:</label><br>
-      <input type="text" id="firstName" name="firstName" value="<?php print($firstName1); ?>"><br>
-      <label for="lname">Etternavn:</label><br>
-      <input type="text" id="lastName" name="lastName" value="<?php print($lastName1); ?>"><br>
-      <label for="email">Email:</label><br>
-      <input type="text" id="email" name="email" value="<?php print($email1); ?>"><br>
-     
-     
-     
-      <label for="role">Rolle:</label><br>
-      <select name="role">
-      <?php selectedRole($role1) ?>
-      </select>
-      <br><br>
-
-      <label for="status">Status:</label><br>
-      <select name="status">
-      <?php status($status1);?> 
-      </select><br>
+        <label for="fname">Brukernavn:</label><br>
+        <input type="text" id="userName" name="userName" value="<?php print($userName1); ?>" ><br>
+        <label for="fname">Fornavn:</label><br>
+        <input type="text" id="firstName" name="firstName" value="<?php print($firstName1); ?>"><br>
+        <label for="lname">Etternavn:</label><br>
+        <input type="text" id="lastName" name="lastName" value="<?php print($lastName1); ?>"><br>
+        <label for="email">Email:</label><br>
+        <input type="text" id="email" name="email" value="<?php print($email1); ?>"><br>
       
-      <img src="../../img/users/<?php print($userUserID.'/'.$image);?>" height="100px"/>
-      <select name='image'>
-          <option value="imagedel">Slett bilde</option>
-          <option value="" selected> Behold bilde</option>
-      </select><br>
-
       
-
-
-
-      <input type="submit" value="Endre" name="submit" id="submit"/>
       
+        <label for="role">Rolle:</label><br>
+        <select name="role">
+        <?php selectedRole($role1) ?>
+        </select>
+        <br><br>
 
-    </form>
-
-    <?php
-      if(isset($_POST['submit'])){
-        $newUserName=$_POST['userName'];
-        $newFirstName=$_POST['firstName'];
-        $newLastName=$_POST['lastName'];
-        $newEmail=$_POST['email'];
-
-        $newStatus=$_POST['status'];
-        $newRole=$_POST['role'];
+        <label for="status">Status:</label><br>
+        <select name="status">
+        <?php status($status1);?> 
+        </select><br>
         
-        $delIMG=$_POST['image'];
+        <img src="../../img/users/<?php print($userUserID.'/'.$image);?>" height="100px"/>
+        <select name='image'>
+            <option value="imagedel">Slett bilde</option>
+            <option value="" selected> Behold bilde</option>
+        </select><br>
+
+        
 
 
 
+        <input type="submit" value="Endre" name="submit" id="submit"/>
+        
 
-        if ($delIMG == "imagedel") {
+      </form>
+
+      <?php
+        if(isset($_POST['submit'])){
+          $newUserName=$_POST['userName'];
+          $newFirstName=$_POST['firstName'];
+          $newLastName=$_POST['lastName'];
+          $newEmail=$_POST['email'];
+
+          $newStatus=$_POST['status'];
+          $newRole=$_POST['role'];
           
-          // Oppdater alle felt i database, samt slette fil fra server
-          $query = "UPDATE users SET firstName = '$newFirstName', lastName = '$newLastName', email = '$newEmail', userName = '$newUserName', role = '$newRole', status = '$newStatus', image='' WHERE userID = '$selectedUserID';";
-
-          mysqli_query($db,$query) or ("<meta http-equiv='refresh' content='0;url=http://opheimpi.zapto.org/www/sda/reko/users/moderator/editUser.php?success=1'>") and die;
-          print("<meta http-equiv='refresh' content='0;url=http://opheimpi.zapto.org/www/sda/reko/users/moderator/editUser.php?success=1'>");
-          
-          
-
-          $path="/var/www/html/www/sda/reko/img/users/".$userID.'/'.$image;
-          unlink($path) or ("<meta http-equiv='refresh' content='0;url=http://opheimpi.zapto.org/www/sda/reko/users/moderator/editUser.php?success=1'>") and die;
-          print("<meta http-equiv='refresh' content='0;url=http://opheimpi.zapto.org/www/sda/reko/users/moderator/editUser.php?success=1'>");
-
-          
+          $delIMG=$_POST['image'];
 
 
+
+
+          if ($delIMG == "imagedel") {
+            
+            // Oppdater alle felt i database, samt slette fil fra server
+            $query = "UPDATE users SET firstName = '$newFirstName', lastName = '$newLastName', email = '$newEmail', userName = '$newUserName', role = '$newRole', status = '$newStatus', image='' WHERE userID = '$selectedUserID';";
+
+            mysqli_query($db,$query) or ("<meta http-equiv='refresh' content='0;url=http://opheimpi.zapto.org/www/sda/reko/users/moderator/editUser.php?success=1'>") and die;
+            print("<meta http-equiv='refresh' content='0;url=http://opheimpi.zapto.org/www/sda/reko/users/moderator/editUser.php?success=1'>");
+            
+            
+
+            $path="/var/www/html/www/sda/reko/img/users/".$userID.'/'.$image;
+            unlink($path) or ("<meta http-equiv='refresh' content='0;url=http://opheimpi.zapto.org/www/sda/reko/users/moderator/editUser.php?success=1'>") and die;
+            print("<meta http-equiv='refresh' content='0;url=http://opheimpi.zapto.org/www/sda/reko/users/moderator/editUser.php?success=1'>");
+
+            
+
+
+          }
+          if(!$delIMG){
+            $query = "UPDATE users SET firstName = '$newFirstName', lastName = '$newLastName', email = '$newEmail', userName = '$newUserName', role = '$newRole', status = '$newStatus', userID = '$selectedUserID' WHERE userID = '$selectedUserID';";      
+            
+            mysqli_query($db,$query) or ("<meta http-equiv='refresh' content='0;url=http://opheimpi.zapto.org/www/sda/reko/users/moderator/editUser.php?success=1'>") and die;
+            
+            print("<meta http-equiv='refresh' content='0;url=http://opheimpi.zapto.org/www/sda/reko/users/moderator/editUser.php?success=1>");
+            
+            
+          }
         }
-        if(!$delIMG){
-          $query = "UPDATE users SET firstName = '$newFirstName', lastName = '$newLastName', email = '$newEmail', userName = '$newUserName', role = '$newRole', status = '$newStatus', userID = '$selectedUserID' WHERE userID = '$selectedUserID';";      
-          
-          mysqli_query($db,$query) or ("<meta http-equiv='refresh' content='0;url=http://opheimpi.zapto.org/www/sda/reko/users/moderator/editUser.php?success=1'>") and die;
-          
-          print("<meta http-equiv='refresh' content='0;url=http://opheimpi.zapto.org/www/sda/reko/users/moderator/editUser.php?success=1>");
-          
-          
-        }
-      }
-      ?>
+        ?>
+  </div>
 </div>
-    </div>
 </html>
