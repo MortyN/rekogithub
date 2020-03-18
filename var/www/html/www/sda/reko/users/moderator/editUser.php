0396fa011c65c34106b314dee4a704d4a49f9527
hakonopheim <hakonopheim@asusphone.home>
2020-03-18 12:40:18 +0100

    tes

--- var/www/html/www/sda/reko/users/moderator/editUser.php
+++ var/www/html/www/sda/reko/users/moderator/editUser.php
@@ -1,6 +1,7 @@
 <?php
 
 include ('control.php');
+include('userFuntion.php');
 $selectedUserID = $_GET['userID'];
 
   $sql = "SELECT * FROM users WHERE userID='$selectedUserID';";
@@ -10,14 +11,14 @@ $selectedUserID = $_GET['userID'];
   
   
  
-    $row = mysqli_fetch_array($query);
+    $row = mysqli_fetch_array($sqlQuery);
 
-    $firstName=$row["firstName"];
-    $lastName=$row["lastName"];
-    $email=$row["email"];
-    $userName=$row["userName"];
-    $role=$row["role"];
-    $status=$row["status"];
+    $firstName1=$row["firstName"];
+    $lastName1=$row["lastName"];
+    $email1=$row["email"];
+    $userName1=$row["userName"];
+    $role1=$row["role"];
+    $status1=$row["status"];
     $userUserID = $row["userID"];
 
   
@@ -26,25 +27,52 @@ $selectedUserID = $_GET['userID'];
 
 <html>
 <div class="profileeditor">
-    <form method="post" enctype="multipart/form-data" action="">
+    <form method="post" action="">
       <label for="fname">Brukernavn:</label><br>
-      <input type="text" id="userName" name="userName" value="<?php print($userName); ?>" readonly><br>
+      <input type="text" id="userName" name="userName" value="<?php print($userName1); ?>" readonly><br>
       <label for="fname">Fornavn:</label><br>
-      <input type="text" id="firstName" name="firstName" value="<?php print($firstName); ?>"><br>
+      <input type="text" id="firstName" name="firstName" value="<?php print($firstName1); ?>"><br>
       <label for="lname">Etternavn:</label><br>
-      <input type="text" id="lastName" name="lastName" value="<?php print($lastName); ?>"><br>
-      <label for="lname">Email:</label><br>
-      <input type="text" id="email" name="email" value="<?php print($email); ?>"><br>
-      <label for="lname">Rolle:</label><br>
-      <input type="text" id="role" name="role" value="<?php print($role); ?>"><br>
+      <input type="text" id="lastName" name="lastName" value="<?php print($lastName1); ?>"><br>
+      <label for="email">Email:</label><br>
+      <input type="text" id="email" name="email" value="<?php print($email1); ?>"><br>
+     
+     
+     
+      <label for="role">Rolle:</label><br>
+      <select name="role">
+      <?php selectedRole($role) ?>
+      </select>
+
+      <label for="status">Status:</label><br>
+      <select name="status">
+      <?php status($status1);?> 
+      </select><br>
       
-      <img src="../img/users/<?php print($userID.'/'.$image);?>" height="100px"/>
-    <select id='image'>
-        <option value="imagedel">Slett bilde</option>
-    </select><br>
+      <img src="../img/users/<?php print($userUserID.'/'.$image);?>" height="100px"/>
+      <select name='image'>
+          <option value="imagedel">Slett bilde</option>
+          <option value="" selected> Behold bilde</option>
+      </select><br>
+
+
       <input type="submit"  value="Endre" name="editProfile" id="editProfile">
       
 
     </form>
+
+    <?php
+      if(ïsset($_POST['editProfile'])){
+        $newUserName=$_POST['userName'];
+        $newFirstName=$_POST['firstName'];
+        $newLastName=$_POST['lastName'];
+        $newEmail=$_POST['email'];
+
+        $newStatus=$_POST['status'];
+        $delIMG=$_POST['image'];
+
+        
+      }
+      ?>
 </div>
 </html>
