05de81d3fadbc778759cbf88d3ac1fee8a0fc43d
hakonopheim <hakonopheim@asusphone.home>
2020-03-19 17:53:38 +0100

    test

--- var/www/html/www/sda/reko/users/commerce/dashboard.php
+++ var/www/html/www/sda/reko/users/commerce/dashboard.php
@@ -3,7 +3,31 @@ include("control.php");
 ?>
 
 
+<div class="dashboard_content">
 
+
+    <div class="dashboardContainer">
+    
+        <div class="dashboard_info">
+            Informasjon
+        </div>
+        
+        <div class="dashboard_orders">
+            Ordre
+        </div>
+        
+        <div class="dashboard_news">
+            Nyheter
+        </div>
+
+    </div>
+
+
+
+
+
+
+</div>
 
 
 
