d1ba922ed6fe3f20f99717ba8769cac879042e58
hakonopheim <hakonopheim@asusphone.home>
2020-03-20 11:37:55 +0100

    test

--- var/www/html/www/sda/reko/users/customer/dashboard.php
+++ var/www/html/www/sda/reko/users/customer/dashboard.php
@@ -32,6 +32,7 @@ include("control.php");
                      INNER JOIN productsOrders
                      ON orders.orderID = productsOrders.orderID
                      WHERE orders.customerID = '$userID'
+                     ORDER BY orders.date,
                      GROUP BY productsOrders.orderID
                      LIMIT 5;";
 
