3adb65f9b466962fc76acdb419c9d3ba7b0a4fa2
hakonopheim <hakonopheim@asusphone.home>
2020-03-20 21:42:22 +0100

    test

--- var/www/html/www/sda/reko/faq.php
+++ var/www/html/www/sda/reko/faq.php
@@ -1,3 +1,4 @@
+
 <?php include("meny.php"); ?>
 <div class="overview_info">
 <h1>Ofte stilte spørsmål</h1>
@@ -22,3 +23,31 @@
 </div>
 
 </div>
+
+
+<style>
+.panel {
+  padding: 0 18px;
+  background-color: white;
+  max-height: 0;
+  overflow: hidden;
+  transition: max-height 0.2s ease-out;
+}
+</style>
+
+<script>
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+</script>
