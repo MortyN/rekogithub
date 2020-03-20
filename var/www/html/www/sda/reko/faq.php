354469c81319941d38518862ca02a471d5cd1028
hakonopheim <hakonopheim@asusphone.home>
2020-03-20 21:50:07 +0100

    test

--- var/www/html/www/sda/reko/faq.php
+++ var/www/html/www/sda/reko/faq.php
@@ -1,7 +1,29 @@
 
-<?php include("meny.php"); ?>
 
 
+<div class="overview_info">
+<h1>Ofte stilte spørsmål</h1>
+<p>Under finner du ofte stilte spørsmål og svar, kanskje du finner svar på det du lurer på</p> <br /> <br />
+
+<div class=nycount>
+<button class="accordion"><h2>Hvordan vet jeg hva jeg har bestilt?</h2></button>
+<div class="panel">
+<p>Dine bestillinger blir automatisk samlet sammen og sendt som èn ordre til din e-post. På denne måten blir det enkelt å se hva man har bestilt, og hvem man har bestilt fra</p> <br />
+</div>
+
+<button class="accordion"><h2>Hvor betaler jeg?</h2></button>
+<div class="panel">
+<p>All betaling foregår direkte mellom deg og din leverandør når du henter din bestilling</p> <br />
+</div>
+
+<button class="accordion"><h2>Hvor henter jeg det jeg har bestilt?</h2>
+<div class="panel">
+<p>Hver andre uke samles kunder og leverandører i kjelleren til Per og koser seg.</p>
+</div>
+
+</div>
+
+</div>
 
 
 <style>
