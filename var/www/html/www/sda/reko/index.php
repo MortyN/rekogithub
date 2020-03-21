09f53ed0f30f4c57ac8ef8b241fa7ff5d711cdea
hakonopheim <hakonopheim@asusphone.home>
2020-03-21 12:54:41 +0100

    test

--- var/www/html/www/sda/reko/index.php
+++ var/www/html/www/sda/reko/index.php
@@ -13,9 +13,9 @@
                 
 
         <div class="section">
-         <div class="section_content" contenteditable="true">
+         <div class="section_content">
                 <h1> Hva er REKO? </h1>
-                <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab, laborum.</p>
+                <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus commodi blanditiis amet. Reiciendis commodi fugit perspiciatis! Quo, deserunt quas aspernatur quos minima illo neque provident. Quam expedita veritatis libero aut eligendi fuga quibusdam, vero officiis eius harum repellat nemo est possimus voluptatibus debitis inventore nihil laboriosam quod animi quos error. Amet suscipit error adipisci saepe quam impedit odit a modi laudantium laborum nisi asperiores molestias deserunt explicabo, enim ut alias. Tempora praesentium incidunt reiciendis. Hic adipisci illo excepturi officiis vel ipsam, aspernatur voluptatibus nobis voluptate libero quae voluptatem voluptates repellat quisquam, nihil laudantium unde molestiae, temporibus sint inventore quam soluta. Asperiores saepe nobis atque harum illum blanditiis reiciendis delectus optio ea odit culpa necessitatibus est expedita consequuntur placeat possimus numquam, laborum quae laudantium fuga et cupiditate tempore magnam! Aliquam, pariatur fugit, nisi animi saepe reiciendis temporibus voluptates deleniti quo odio tempore recusandae. Consequatur, sunt. Libero provident, magnam neque eaque possimus dolore doloribus explicabo quis alias, tempora molestias tenetur asperiores enim. Totam dolorum in magnam nemo cumque nobis ducimus consectetur, qui reprehenderit animi aspernatur quaerat soluta tempora numquam? Nesciunt facilis amet, hic vel voluptatum quos, veritatis nulla autem maxime asperiores quia tempora aliquam commodi? Quisquam ex accusamus molestias, porro dolor hic.</p>
             </div>
         </div>
 
