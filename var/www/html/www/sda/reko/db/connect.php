020f5e591eaea59de7c9dfecbfb9092fba9c0f68
MortyN <macgeekfromapple@gmail.com>
2020-03-21 19:05:39 +0100

    delet plz come to brasil

--- var/www/html/www/sda/reko/db/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host="localhost";
-$user="hakonopheim";
-$password="5k0th31a1";
-$database="reko";
-
-$db=mysqli_connect($host,$user,$password,$database) or die ("ikke kontakt med database-server");
-
-?>
