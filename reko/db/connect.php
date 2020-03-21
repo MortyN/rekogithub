adda9f93ce9080c2d9536848d32278e132bcb222
hakonopheim <hakonopheim@asusphone.home>
2020-03-21 19:43:58 +0100

    test

--- reko/db/connect.php
+++ reko/db/connect.php
@@ -1,8 +1,8 @@
 <?php
 $host="localhost";
-$user="hakonopheim";
-$password="5k0th31a1";
-$database="reko";
+$user="opheimoc_reko";
+$password="rekodev69";
+$database="opheimoc_reko";
 
 $db=mysqli_connect($host,$user,$password,$database) or die ("ikke kontakt med database-server");
 
