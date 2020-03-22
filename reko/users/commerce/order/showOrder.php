143cd5c7a766062ea22371f6ced12158fe6169be
hakonopheim <hakonopheim@asusphone.home>
2020-03-22 21:08:39 +0100

    test

--- reko/users/commerce/order/showOrder.php
+++ reko/users/commerce/order/showOrder.php
@@ -131,11 +131,8 @@ if (isset($_GET['success'])) {
             break;
         }   
 
-        if(!$mail->Send()) {
-            echo "Mailer Error: " . $mail->ErrorInfo;
-         } else {
-            echo "Message has been sent";
-         }
+        $mail->Send()
+           
          $mail->ClearAddresses();
 
         //print("<meta http-equiv='refresh' content='0;URL=http://opheimpi.zapto.org/www/sda/reko/users/commerce/order/showOrder.php?orderID=$orderID&success=updateOK'/>");
