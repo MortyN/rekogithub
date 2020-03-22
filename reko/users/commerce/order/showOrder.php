2fd11a8ef412b00c14af93a6032ed20998cc8c72
hakonopheim <hakonopheim@asusphone.home>
2020-03-22 23:45:44 +0100

    test

--- reko/users/commerce/order/showOrder.php
+++ reko/users/commerce/order/showOrder.php
@@ -319,10 +319,15 @@ if (isset($_GET['success'])) {
             break;
         }   
 
-        $mail->Send();
+        if(!$mail->Send()){
+            print("<meta http-equiv='refresh' content='1;URL=http://opheimpi.zapto.org/www/sda/reko/users/commerce/order/showOrder.php?orderID=$orderID&success=updateOK'/>");
+        }
+        else{
+            print("<meta http-equiv='refresh' content='1;URL=http://opheimpi.zapto.org/www/sda/reko/users/commerce/order/showOrder.php?orderID=$orderID&success=updateOK'/>");
+        }
         $mail->ClearAddresses();
 
-        print("<meta http-equiv='refresh' content='1;URL=http://opheimpi.zapto.org/www/sda/reko/users/commerce/order/showOrder.php?orderID=$orderID&success=updateOK'/>");
+        
         
         }
         ?>
