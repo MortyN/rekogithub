7baa3f3a8462f476c12f3dd94c1b25d1737cde1d
hakonopheim <hakonopheim@asusphone.home>
2020-03-22 21:11:28 +0100

    test

--- reko/users/mail-config.php
+++ reko/users/mail-config.php
@@ -1,17 +1,20 @@
 <?php
 
 require("/usr/share/php/libphp-phpmailer/src/PHPMailer.php");
- require("/usr/share/php/libphp-phpmailer/src/SMTP.php");
+require("/usr/share/php/libphp-phpmailer/src/SMTP.php");
 
- $mail = new PHPMailer\PHPMailer\PHPMailer();
-    $mail->IsSMTP(); // enable SMTP
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+   $mail->IsSMTP(); // enable SMTP
 
-    $mail->SMTPDebug = 2; // debugging: 1 = errors and messages, 2 = messages only
-    $mail->SMTPAuth = true; // authentication enabled
-    $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
-    $mail->Host = "reko.opheim.as";
-    $mail->Username = "noreply@reko.opheim.as";
-    $mail->Password = "rekodev69";
-    $mail->Port = 465; // or 587
-    $mail->IsHTML(true);
+   $mail->SMTPDebug = 2; // debugging: 1 = errors and messages, 2 = messages only
+   $mail->SMTPAuth = true; // authentication enabled
+   $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
+   $mail->Host = "reko.opheim.as";
+   $mail->Port = 465; // or 587
+   $mail->IsHTML(true);
+  
+   $mail->Username = "noreply@reko.opheim.as";
+   $mail->Password = "rekodev69";
+   $mail->SetFrom("noreply@reko.opheim.as");
+   
     ?>
