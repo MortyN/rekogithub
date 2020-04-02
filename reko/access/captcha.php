9e9d65c6dec8f593b6f72275444bd7382a706289
hakonopheim <hakonopheim@Hakons-MacBook-Pro.local>
2020-04-02 12:52:43 +0200

    test

--- reko/access/captcha.php
+++ reko/access/captcha.php
@@ -18,7 +18,7 @@
     $text = $display;
 
     // place the font file in the same dir level as the php file
-    $font = 'verdana.ttf';
+    $font = 'Vogue.ttf';
 
     //this function sets the font size, places to the co-ords
     // imagettftext( [image], [size], [angle], [x], [y], [color], [fontfile], [text] )
