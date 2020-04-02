d84fe77853728c19761c492c70429e54d578879d
hakonopheim <hakonopheim@Hakons-MacBook-Pro.local>
2020-04-02 14:29:53 +0200

    test

--- reko/commerce/feed.php
+++ reko/commerce/feed.php
@@ -12,63 +12,69 @@ GROUP BY orders.commerceID;";
 $result = mysqli_query($db,$sql) or die ("kan ikke laste feed");
 $xRows = mysqli_num_rows($result);
 print("<div class='feed_container'>");
+
+    if($xRows == 0){
+        print("Ikke noen innlegg i dag niet");
+    }
+    else{
  
-    for($i=1;$i <= $xRows; $i++ ){
-        $part = mysqli_fetch_array($result);
-            $firstName = $part["firstName"];
-            $lastName = $part["lastName"];
-            $profileIMG = $part["profileIMG"];
-            $shortText = nl2br($part["shortText"]);
-            $postIMG = $part["postIMG"];
-            $userID = $part["userID"];
-            $orderCount = $part["orderCount"]
-            
-            ?>
+        for($i=1;$i <= $xRows; $i++ ){
+            $part = mysqli_fetch_array($result);
+                $firstName = $part["firstName"];
+                $lastName = $part["lastName"];
+                $profileIMG = $part["profileIMG"];
+                $shortText = nl2br($part["shortText"]);
+                $postIMG = $part["postIMG"];
+                $userID = $part["userID"];
+                $orderCount = $part["orderCount"]
+                
+                ?>
 
 
 
-            <div class="post">
-                        
-                    <div class="profileInfo">
-                    <?php
-                            if(!$profileIMG){
-                                print("<a href='profile.php?ID=".$userID."'><img src='../img/default-profile.jpg'/> </a>");
-                                
-                            }
-                            else{
-                                print("<a href='profile.php?ID=".$userID."'><img src='../img/users/".$userID."/".$profileIMG."'/> </a>");
-                            }
-
-                            ?>
-                                     
-                                 <p> <?php print($firstName." ".$lastName); ?> </p>
-                    </div>
-                                <div class="content">
-                                    <div class="ad__pic">
-                                        <img src="../img/users/<?php print($userID.'/'.$postIMG);?>"/>
-                                    </div>
-                                    <div class="ad_shortinfo">
-                                        <p><?php print($shortText);?></p>
-                                        
-                                    </div>
-                                    <a href='profile.php?ID=<?php print($userID); ?>'><button class="feedBTN">Bestill nå!</button>
-                                    </a>
-                                    <?php 
-                                    if ( $orderCount == 1){
-                                        print("<br><br><br><p>".$orderCount." bestilling</p>"); 
-                                    }
-                                    if ($orderCount > 1){
-                                        print("<br><br><br><p>".$orderCount." bestillinger</p>"); 
-                                    }
-                                    ?>
-
+                <div class="post">
                             
-                                </div>                                
+                        <div class="profileInfo">
+                        <?php
+                                if(!$profileIMG){
+                                    print("<a href='profile.php?ID=".$userID."'><img src='../img/default-profile.jpg'/> </a>");
+                                    
+                                }
+                                else{
+                                    print("<a href='profile.php?ID=".$userID."'><img src='../img/users/".$userID."/".$profileIMG."'/> </a>");
+                                }
+
+                                ?>
+                                        
+                                    <p> <?php print($firstName." ".$lastName); ?> </p>
+                        </div>
+                                    <div class="content">
+                                        <div class="ad__pic">
+                                            <img src="../img/users/<?php print($userID.'/'.$postIMG);?>"/>
+                                        </div>
+                                        <div class="ad_shortinfo">
+                                            <p><?php print($shortText);?></p>
+                                            
+                                        </div>
+                                        <a href='profile.php?ID=<?php print($userID); ?>'><button class="feedBTN">Bestill nå!</button>
+                                        </a>
+                                        <?php 
+                                        if ( $orderCount == 1){
+                                            print("<br><br><br><p>".$orderCount." bestilling</p>"); 
+                                        }
+                                        if ($orderCount > 1){
+                                            print("<br><br><br><p>".$orderCount." bestillinger</p>"); 
+                                        }
+                                        ?>
+
+                                
+                                    </div>                                
 
 
-            </div>
+                </div>
 
-<?php
+    <?php
+    }
 }
 ?>
 </div>
