46528f955094308f710bc278e8203491c7295373
hakonopheim <hakonopheim@Hakons-MacBook-Pro.local>
2020-04-02 16:52:39 +0200

    test

--- reko/users/commerce/chat/chat_overview.php
+++ reko/users/commerce/chat/chat_overview.php
@@ -1,2 +1,18 @@
 <?php include("../control.php"); ?>
 
+    <div class="dashboard_content">
+        <div class="chatContainer">
+            <div class="chatInfo">
+                <h2>Dine samtaler</h2>
+                <p>Til venstre har du en oversikt over alle dine pågående samtaler, klikk på navnet til kunden eller leverandøren, for å  fortsette samtalen.
+                    Til høyre ser du en oversikt over alle Leverandører og Kunder som er registrert i din REKO-ring, klikke på en av dem for å starte en ny samtale.
+                </p>
+            </div>
+            <div class="activeChat">
+
+            </div>
+            <div class="newChat">
+                
+            </div>
+        </div>
+    </div>
