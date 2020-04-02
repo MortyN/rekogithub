76c5386f75133d6f0f5a5da2447807d0d272c318
hakonopheim <hakonopheim@Hakons-MacBook-Pro.local>
2020-04-02 18:21:03 +0200

    tets¨

--- reko/users/commerce/chat/chat_overview.php
+++ reko/users/commerce/chat/chat_overview.php
@@ -85,7 +85,7 @@
                         break;
 
                     }
-                    print("<tr><td><a href='/chat_box.php?chatID=$chatID'>$firstName $lastName</a></td><tr><td><a href='/chat_box.php?chatID=$chatID'>$type</a></td>¨</tr>");
+                    print("<tr><td><a href='/chat_box.php?chatID=$chatID'>$firstName $lastName</a></td> <td><a href='/chat_box.php?chatID=$chatID'>$type</a></td></tr>");
                 }
                 ?>
                 </table>
