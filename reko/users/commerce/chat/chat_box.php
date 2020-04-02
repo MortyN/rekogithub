089fbff96d5634e905612edf4b3c75d2e2332c84
hakonopheim <hakonopheim@Hakons-MacBook-Pro.local>
2020-04-02 18:29:56 +0200

    test

---
+++ reko/users/commerce/chat/chat_box.php
@@ -0,0 +1,28 @@
+<div class="dashboard_content">
+        <div class="chatBoxContainer">
+            <div class="chatBoxInfo">
+                <h2>Dine samtaler</h2>
+                <p>Til venstre har du en oversikt over alle dine pågående samtaler, klikk på navnet til kunden eller leverandøren, for å  fortsette samtalen.
+                    Til høyre ser du en oversikt over alle Leverandører og Kunder som er registrert i din REKO-ring, klikke på en av dem for å starte en ny samtale.
+                </p>
+            </div>
+            <div class="chat">
+                <div class="messages">
+                    <div class="msg msg-left">left</div>
+                    <div class="msg msg-left">left</div>
+                    <div class="msg msg-right">right</div>
+                    <div class="msg msg-left">left</div>
+                    <div class="msg msg-right">right</div>
+                    <div class="msg msg-left">left</div>
+                    <div class="msg msg-right">right</div>
+                    <div class="msg msg-left">left</div>
+                    <div class="msg msg-left">left</div>
+                    <div class="msg msg-right">right</div>
+                </div>  
+
+                <form class="msgForm" method="POST">
+                    <input type="text" class="newMsg" name="newMsg"/>
+                    <input type="submit" class="sendMsg" name="sendMsg"/>
+                </form>
+                    
+            </div>
