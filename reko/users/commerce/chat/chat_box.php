3180238abb06a9d221fa62b083394119352dc542
hakonopheim <hakonopheim@Hakons-MacBook-Pro.local>
2020-04-02 23:24:29 +0200

    test

--- reko/users/commerce/chat/chat_box.php
+++ reko/users/commerce/chat/chat_box.php
@@ -10,66 +10,42 @@
             <div class="chat">
 
                 <div class="messages">
-                    <div class="msgcontainer">
+                   
+                <?php 
+                        $chatID = $_GET["chatID"];
+                        $sql ="SELECT users.userID, users.firstName,users.lastName, chat_message.message, chat_message.date
+                        FROM users
+                        INNER JOIN chat_message
+                        ON chat_message.msg_from = users.userID
+                        WHERE chatID = $chatID;";
 
-                        <div class="msgName msgName-left">Fornavn Etternavn</div>
+                        $result = mysqli_query($db,$sql) or die("kan ikke hente meldinger");
+                        $rows = mysqli_num_rows($result);
 
-                        <div class="msg msg-left"> dolorem quam, sed quas beatae! Blanditiis odit ducimus veritatis culpa est esse.</div>
-                        <div class="msgDate msgDate-left">14:34 13/05/2020</div>
-                    </div>
-                    <div class="msgcontainer">
+                        for($k=1; $k<=$rows; $k++){
+                            $row = mysqli_fetch_array(result);
 
-                        <div class="msgName msgName-right">Fornavn Etternavn</div>
-                        
-                        <div class="msg msg-right">est odit explicabo! Ducimus aliquam alias illum est nam ab repudiandae?</div>
-                        <div class="msgDate msgDate-right">14:34 13/05/2020</div>
+                            $userUserID = $row['userID'];
+                            $firstName = $row['firstName'];
+                            $lastName = $row['lastName'];
+                            $message = $row['message'];
+                            $date = $row['date'];
+                            
+                            if($userUserID == $userID){
+                                $class = "right";
+                            }
+                            else{
+                                $class = "left";
+                            }?>
+                            <div class="msgcontainer">
 
-                    </div>
-                    <div class="msgcontainer">
+                            <div class="msgName msgName-<?php print("$class"); ?> "><p><?php print($firstName." ".$lastName); ?> </p></div>
 
-                        <div class="msgName msgName-left">Fornavn Etternavn</div>
-
-                        <div class="msg msg-left"> dolorem quam, sed quas beatae! Blanditiis odit ducimus veritatis culpa est esse.</div>
-                        <div class="msgDate msgDate-left">14:34 13/05/2020</div>
-                    </div>
-                    <div class="msgcontainer">
-
-                        <div class="msgName msgName-right">Fornavn Etternavn</div>
-                        
-                        <div class="msg msg-right">est odit explicabo! Ducimus aliquam alias illum est nam ab repudiandae?</div>
-                        <div class="msgDate msgDate-right">14:34 13/05/2020</div>
-
-                    </div>
-                    <div class="msgcontainer">
-
-                        <div class="msgName msgName-left">Fornavn Etternavn</div>
-
-                        <div class="msg msg-left"> dolorem quam, sed quas beatae! Blanditiis odit ducimus veritatis culpa est esse.</div>
-                        <div class="msgDate msgDate-left">14:34 13/05/2020</div>
-                    </div>
-                    <div class="msgcontainer">
-
-                        <div class="msgName msgName-right">Fornavn Etternavn</div>
-                        
-                        <div class="msg msg-right">est odit explicabo! Ducimus aliquam alias illum est nam ab repudiandae?</div>
-                        <div class="msgDate msgDate-right">14:34 13/05/2020</div>
-
-                    </div>
-                    <div class="msgcontainer">
-
-                        <div class="msgName msgName-left">Fornavn Etternavn</div>
-
-                        <div class="msg msg-left"> dolorem quam, sed quas beatae! Blanditiis odit ducimus veritatis culpa est esse.</div>
-                        <div class="msgDate msgDate-left">14:34 13/05/2020</div>
-                    </div>
-                    <div class="msgcontainer">
-
-                        <div class="msgName msgName-right">Fornavn Etternavn</div>
-                        
-                        <div class="msg msg-right">est odit explicabo! Ducimus aliquam alias illum est nam ab repudiandae?</div>
-                        <div class="msgDate msgDate-right">14:34 13/05/2020</div>
-
-                    </div>
+                            <div class="msg msg-left"> <p><?php print("$message"); ?></p></div>
+                            <div class="msgDate msgDate-<?php print("$class");?>"> <p><?php print($date); ?> </p></div>
+                            </div>
+                            <?php
+                        } ?>
                     
                     
                 </div>
@@ -81,7 +57,21 @@
                 </div>  
             </div>
             
-            <!--  --> 
+            <!--  <div class="msgcontainer">
+
+                        <div class="msgName msgName-left">Fornavn Etternavn</div>
+
+                        <div class="msg msg-left"> dolorem quam, sed quas beatae! Blanditiis odit ducimus veritatis culpa est esse.</div>
+                        <div class="msgDate msgDate-left">14:34 13/05/2020</div>
+                    </div>
+                    <div class="msgcontainer">
+
+                        <div class="msgName msgName-right">Fornavn Etternavn</div>
+                        
+                        <div class="msg msg-right">est odit explicabo! Ducimus aliquam alias illum est nam ab repudiandae?</div>
+                        <div class="msgDate msgDate-right">14:34 13/05/2020</div>
+
+                    </div> --> 
 
 
 
