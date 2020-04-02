52f482fd6db0861c9b5b02cbf34fbfa92a356d18
hakonopheim <hakonopheim@Hakons-MacBook-Pro.local>
2020-04-02 11:10:38 +0200

    Co-authored-by: TooHTP <habba007@hotmail.com> Co-authored-by: Kristoffer-usn <latzey93@gmail.com> Co-authored-by: MortyN <macgeekfromapple@gmail.com> Co-authored-by: inception99 <inception99@protonmail.ch>

---
+++ reko/users/moderator/functionsNavbar.js
@@ -0,0 +1,8 @@
+function navBar() {
+    var x = document.getElementById("mainNav");
+    if (x.className === "mainNav") {
+        x.className += " responsive";
+    } else {
+        x.className = "mainNav";
+    }
+}
