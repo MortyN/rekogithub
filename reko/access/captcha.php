38b25ea497d907630c5723dee50c076113c37a83
hakonopheim <hakonopheim@Hakons-MacBook-Pro.local>
2020-04-02 13:27:42 +0200

    test

--- reko/access/captcha.php
+++ reko/access/captcha.php
@@ -6,7 +6,22 @@
     $_SESSION['captchaCheck'] =$num1 + $num2;
     $display = $num1."+".$num2."=";
 
-    $img = imagecreate( 75, 38 );
+    $my_img = imagecreate( 75, 38 );
+    $background = imagecolorallocate( $my_img, 54, 166, 61 );
+    $text_colour = imagecolorallocate( $my_img, 255, 255, 255 );
+    $line_colour = imagecolorallocate( $my_img, 128, 255, 0 );
+    imagestring( $my_img, 4, 30, 25, "$num1 + $num2 =", $text_colour );
+    imagesetthickness ( $my_img, 5 );
+    imageline( $my_img, 30, 45, 165, 45, $line_colour );
+
+    header( "Content-type: image/png" );
+    imagepng( $my_img );
+    imagecolordeallocate( $line_color );
+    imagecolordeallocate( $text_color );
+    imagecolordeallocate( $background );
+    imagedestroy( $my_img );
+
+    /*$img = imagecreate( 75, 38 );
 
     // choose a bg color, you can play with the rgb values
     // imagecolorallocate( [image], [red], [green], [blue] )
@@ -35,5 +50,6 @@
     //destroys used resources
     imagecolordeallocate( $text_color );
     imagecolordeallocate( $background );
-    imagedestroy( $img );
+    imagedestroy( $img );*/
 ?>
+
