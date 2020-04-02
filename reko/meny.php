44b3e20633142e729aba256c14d09c1caeb19ad8
hakonopheim <hakonopheim@Hakons-MacBook-Pro.local>
2020-04-02 11:36:09 +0200

    test

--- reko/meny.php
+++ reko/meny.php
@@ -19,7 +19,7 @@ include("/var/www/html/www/sda/reko/db/connect.php");
 
 
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/cookie-bar/cookiebar-latest.min.js?forceLang=no&theme=flying&always=1&noGeoIp=1&refreshPage=1&showNoConsent=1&hideDetailsBtn=1&showPolicyLink=1&remember=30&privacyPage=http%3A%2F%2Fopheimpi.zapto.org%2Fwww%2Fsda%2Freko%2Fpolicy.php"></script>
-<script type="text/javascript" src="functionsNavbar.js"></script>
+<script type="text/javascript" src="/www/sda/reko/functionsNavbar.js"></script>
 
 
 </head>
