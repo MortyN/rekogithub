fadbb3fc75168cbffc24a077858fd6d818573c3f
hakonopheim <hakonopheim@Hakons-MacBook-Pro.local>
2020-04-02 13:54:05 +0200

    test

--- reko/access/regUser.php
+++ reko/access/regUser.php
@@ -45,12 +45,13 @@
            
 
 <?php
-    $check = $_SESSION['check'];
-    print($check);
+    
     if (isset($_POST["submit"]))
     {
         
         
+        $check = $_SESSION['check'];
+        print($check);
         $firstName = $_POST["firstName"];
         $lastName = $_POST["lastName"];
         $eMail = $_POST["eMail"];
