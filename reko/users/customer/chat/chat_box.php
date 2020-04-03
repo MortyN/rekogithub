086096e338ffaa16e81910f3f35cf8ec14dd0ed9
hakonopheim <hakonopheim@Hakons-MacBook-Pro.local>
2020-04-03 09:45:11 +0200

    tets

---
+++ reko/users/customer/chat/chat_box.php
@@ -0,0 +1,74 @@
+<?php include("../control.php"); ?>
+<div class="dashboard_content">
+        <div class="chatBoxContainer">
+            <div class="chatBoxInfo">
+                <h2>Dine samtaler</h2>
+                <p>Til venstre har du en oversikt over alle dine pågående samtaler, klikk på navnet til kunden eller leverandøren, for å  fortsette samtalen.
+                    Til høyre ser du en oversikt over alle Leverandører og Kunder som er registrert i din REKO-ring, klikke på en av dem for å starte en ny samtale.
+                </p>
+            </div>
+            <div class="chat">
+
+                <div class="messages">
+                   
+                <?php 
+                        $chatID = $_GET["chatID"];
+                        $sql ="SELECT users.userID, users.firstName,users.lastName, chat_message.message, chat_message.date
+                        FROM users
+                        INNER JOIN chat_message
+                        ON chat_message.msg_from = users.userID
+                        WHERE chatID = $chatID;";
+
+                        $result = mysqli_query($db,$sql) or die("kan ikke hente meldinger");
+                        $rows = mysqli_num_rows($result);
+
+                        for($k=1; $k<=$rows; $k++){
+                            $row = mysqli_fetch_array($result);
+
+                            $userUserID = $row['userID'];
+                            $firstName = $row['firstName'];
+                            $lastName = $row['lastName'];
+                            $message = $row['message'];
+                            $date = $row['date'];
+                            
+                            if($userUserID == $userID){
+                                $class = "right";
+                            }
+                            else{
+                                $class = "left";
+                            }?>
+                            <div class="msgcontainer">
+
+                            <div class="msgName msgName-<?php print($class); ?> "><?php print($firstName." ".$lastName); ?> </div>
+
+                            <div class="msg msg-<?php print($class);?>"> <?php print($message); ?></div>
+                            <div class="msgDate msgDate-<?php print($class);?>"> <?php print($date); ?> </div>
+                            </div>
+                            <?php
+                        } ?>
+                    
+                    
+                </div>
+                <div class="inputMsg">
+                <form class="msgForm" method="POST">
+                    <input type="text" id="newMsg" name="newMsg"/>
+                    <input type="submit" id="sendMsg" name="sendMsg"/>
+                </form>  
+                </div>  
+            </div>
+            
+           <?php
+           if(isset($_POST["sendMsg"])){
+               $newMessage = $_POST["newMsg"];
+               
+               $sql_newMsg = "INSERT INTO chat_message (chatID, msg_from, message, date) VALUES ($chatID,$userID,'$newMessage', NOW());";
+               print("$sql_newMsg");
+               if(mysqli_query($db,$sql_newMsg)){
+                echo "<meta http-equiv='refresh' content='0;url=http://opheimpi.zapto.org/www/sda/reko/users/commerce/chat/chat_box.php?chatID=$chatID'>";
+               }
+               else{
+                   print("Kan ikke sende melding");
+                   
+                   
+               }
+            }
---
+++ reko/users/moderator/chat/chat_overview.php
@@ -0,0 +1,105 @@
+<?php include("../control.php"); ?>
+
+    <div class="dashboard_content">
+        <div class="chatContainer">
+            <div class="chatInfo">
+                <h2>Dine samtaler</h2>
+                <p> Til venstre har du en oversikt over alle dine pågående samtaler, klikk på navnet til kunden eller leverandøren, for å  fortsette samtalen.
+                    Til høyre ser du en oversikt over alle Leverandører og Kunder som er registrert i din REKO-ring, klikke på en av dem for å starte en ny samtale.
+                </p>
+            </div>
+            <div class="activeChat">
+                <table class="chatOverview">
+                <tr>
+                    <th>Navn</th>
+                    <th>Type</th>
+                    <th>Pålogget</th> 
+                </tr>
+                <?php 
+                $sql= "SELECT chat_connection.chatID, users.firstName,users.lastName, users.role, users.last_timestamp AS time
+                FROM users
+                INNER JOIN chat_connection
+                ON users.userID = chat_connection.commerceID
+                WHERE chat_connection.customerID = $userID";
+                
+
+                $result = mysqli_query($db,$sql) or ("Kan ikke hente samtaler akkurat nå.");
+                $num = mysqli_num_rows($result);
+
+                for($i=1; $i<=$num; $i++){
+                    $part=mysqli_fetch_array($result);
+
+                    $firstName = $part["firstName"];
+                    $lastName = $part["lastName"];
+                    $type = $part["role"];
+                    $time = strtotime($part["time"]);
+                    $chatID = $part['chatID'];
+                    
+
+                    switch ($type){
+                        case "commerce":
+                            $type = "Leverandør";
+                        break;
+                        case "customer":
+                            $type = "Kunde";
+                        break;
+                        case "Moderator":
+                            $type = "Moderator";
+                        break;
+
+                    }
+                    if (time() - $time > 15 * 60 || date("Y-m-d", $time) != date("Y-m-d") )  {
+                             $online = "Frakoblet";   
+                        }
+                        else{
+                            $online = "Pålogget";
+                        }
+
+                    
+                    print("<tr><td><a href='http://opheimpi.zapto.org/www/sda/reko/users/commerce/chat/chat_box.php?chatID=$chatID'>$firstName $lastName</a></td> <td><a href='http://opheimpi.zapto.org/www/sda/reko/users/commerce/chat/chat_box.php?chatID=$chatID'>$type</a></td> <td><a href='http://opheimpi.zapto.org/www/sda/reko/users/commerce/chat/chat_box.php?chatID=$chatID'>$online</a></td></tr>");
+                }
+                ?>
+                </table>
+            </div>
+            <div class="newChat">
+            <table class="chatOverview">
+                <tr>
+                    <th>Navn</th>
+                    <th>Type</th>
+                </tr>
+                <?php 
+                $sql= "SELECT firstName,lastName, role FROM users;";
+
+                $result = mysqli_query($db,$sql) or die("Kan ikke hente produkter akkurat nå.");
+                $num = mysqli_num_rows($result);
+
+                for($i=1; $i<=$num; $i++){
+                    $part=mysqli_fetch_array($result);
+
+                    $firstName = $part["firstName"];
+                    $lastName = $part["lastName"];
+                    $type = $part["role"];
+                    
+                    
+
+                    switch ($type){
+                        case "commerce":
+                            $type = "Leverandør";
+                        break;
+                        case "customer":
+                            $type = "Kunde";
+                        break;
+                        case "Moderator":
+                            $type = "Moderator";
+                        break;
+
+                    }
+                    print("<tr><td><a href='http://opheimpi.zapto.org/www/sda/reko/users/commerce/chat/chat_box.php?chatID=$chatID'>$firstName $lastName</a></td> <td><a href='http://opheimpi.zapto.org/www/sda/reko/users/commerce/chat/chat_box.php?chatID=$chatID'>$type</a></td></tr>");
+                }
+                ?>
+                </table>
+
+
+            </div>
+        </div>
+    </div>
