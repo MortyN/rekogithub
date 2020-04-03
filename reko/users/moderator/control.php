fb8aa2612b94fe2185b9dc91a4a265d4b6c71944
hakonopheim <hakonopheim@Hakons-MacBook-Pro.local>
2020-04-03 14:12:47 +0200

    test

--- reko/users/moderator/control.php
+++ reko/users/moderator/control.php
@@ -42,6 +42,8 @@ $del = mysqli_fetch_array($sqlQuery);
         <link rel="icon" href="/www/sda/reko/img/rekologo.png">
         <link rel="stylesheet" href="/www/sda/reko/stylesheet.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css" rel="stylesheet">
+        <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
         <meta charset="UTF-8">
 </head>
 <body class="dashboard">
