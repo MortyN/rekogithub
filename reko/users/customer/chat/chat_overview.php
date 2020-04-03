8076b259179002f1bab2e5a5e3ea0519b3a1fb0e
hakonopheim <hakonopheim@Hakons-MacBook-Pro.local>
2020-04-03 16:12:01 +0200

    test

--- reko/users/customer/chat/chat_overview.php
+++ reko/users/customer/chat/chat_overview.php
@@ -3,12 +3,13 @@
     <div class="dashboard_content">
         <div class="chatContainer">
             <div class="chatInfo">
-                <h2>Dine samtaler</h2>
+                <h2>Send melding</h2>
                 <p> Til venstre har du en oversikt over alle dine pågående samtaler, klikk på navnet til kunden eller leverandøren, for å  fortsette samtalen.
                     Til høyre ser du en oversikt over alle Leverandører og Kunder som er registrert i din REKO-ring, klikke på en av dem for å starte en ny samtale.
                 </p>
             </div>
             <div class="activeChat">
+            <h3>Dine samtaler</h3>
                 <table class="chatOverview">
                 <tr>
                     <th>Navn</th>
@@ -86,6 +87,7 @@
                 </table>
             </div>
             <div class="newChat">
+                <h3>Ny samtale </h3>
             <table class="chatOverview">
                 <tr>
                     <th>Navn</th>
