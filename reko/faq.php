7966666b472eb589610fe9cb17f9a005173c4f2b
hakonopheim <hakonopheim@Hakons-MacBook-Pro.local>
2020-04-08 18:41:10 +0200

    test

--- reko/faq.php
+++ reko/faq.php
@@ -1,6 +1,6 @@
 <?php include("meny.php"); ?>
 
-
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <div class="overview_info">
 <h1>Ofte stilte spørsmål</h1>
 <p>Under finner du ofte stilte spørsmål og svar, kanskje du finner svar på det du lurer på</p> <br /> <br />
