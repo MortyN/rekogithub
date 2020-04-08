1c59ae2dd68dbbdfbbd89da502696192a8837810
hakonopheim <hakonopheim@Hakons-MacBook-Pro.local>
2020-04-08 18:40:20 +0200

    test

--- reko/faq.php
+++ reko/faq.php
@@ -5,64 +5,41 @@
 <h1>Ofte stilte spørsmål</h1>
 <p>Under finner du ofte stilte spørsmål og svar, kanskje du finner svar på det du lurer på</p> <br /> <br />
 
-<div class=nycount>
-<button class="accordion"><h2>Hvordan vet jeg hva jeg har bestilt?</h2></button>
-
-<div class="panel">
-<p>Dine bestillinger blir automatisk samlet sammen og sendt som èn ordre til din e-post. På denne måten blir det enkelt å se hva man har bestilt, og hvem man har bestilt fra</p> <br />
+<h1>Ofte stilte spørsmål</h1>
+<p>Under finner du ofte stilte spørsmål og svar, kanskje du finner svar på det du lurer på</p>
+  
+<button onclick="myFunction('1')" class="question">
+<h2>Hvordan vet jeg hva jeg har bestilt?</h2>
+</button>
+<div id="1" class="w3-container w3-hide qContent">
+  <p>Dine bestillinger blir automatisk samlet sammen og sendt som èn ordre til din e-post. På denne måten blir det enkelt å se hva man har bestilt, og hvem man har bestilt fra</p>
 </div>
 
-<button class="accordion"><h2>Hvor betaler jeg?</h2></button>
-<div class="panel">
-<p>All betaling foregår direkte mellom deg og din leverandør når du henter din bestilling.</p>
+<button onclick="myFunction('2')" class="question">
+<h2>Hvor betaler jeg?</h2>
+</button>
+<div id="2" class="w3-container w3-hide qContent">
+ <p>All betaling foregår direkte mellom deg og din leverandør når du henter din bestilling.</p>
 </div>
 
-<button class="accordion"><h2>Hvor henter jeg det jeg har bestilt?</h2></button>
-<div class="panel">
+<button onclick="myFunction('3')" class="question">
+<h2>Hvor henter jeg det jeg har bestilt?</h2>
+</button>
+<div id="3" class="w3-container w3-hide qContent">
 <p>Hver andre uke samles kunder og leverandører i kjelleren til Per og koser seg.</p>
 </div>
 
 </div>
 
-</div>
-
-
-<style>
-.panel {
-  padding: 0 18px;
-  background-color: whitesmoke;
-  max-height: 0;
-  overflow: hidden;
-  transition: max-height 0.2s ease-out;
-}
-.accordion:after {
-content: '\02795'; /* Unicode character for "plus" sign (+) */
-font-size: 13px;
-color: #777;
-float: right;
-margin-left: 5px;
-}
-
-.active:after {
-content: "\2796"; /* Unicode character for "minus" sign (-) */
-}
-
-</style>
 
 <script>
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  });
+function myFunction(id) {
+  var x = document.getElementById(id);
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
 }
 </script>
 <?php include("footer1.php"); ?>
