9222562a4b63a87356081cae152c9b739523a103
hakonopheim <hakonopheim@chromecast.home>
2020-05-06 18:14:16 +0200

    test

--- reko/users/moderator/userguidance.php
+++ reko/users/moderator/userguidance.php
@@ -3,7 +3,7 @@
 ?>
 <div class="dashboard_content">
 <div>
-    <object data="pdf/brukerveiledningmoderator.pdf" type="application/pdf" width="300" height="200">
+    <object data="pdf/brukerveiledningmoderator.pdf" type="application/pdf" width="80%" height="80%">
         alt : <a href="tpdf/brukerveiledningmoderator.pdf">Brukerveiledning.pdf</a>
     </object>
 </div>
